65e8781ef535cff0017e8d84d2fa150f1cce0144
epic-constructor-9765 <barton.e.marks@gmail.com>
2019-09-09 16:40:11 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,6 +2,11 @@
 
 def meal_choice(veg1, veg2, protein="meat")
   puts "What a nutritious meal!"
+<<<<<<< HEAD
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   return "A plate of #{protein} with #{veg1} and #{veg2}."
+=======
+  puts  "A plate of #{protein} with #{veg1} and #{veg2}."
+  p "A plate of #{protein} with #{veg1} and #{veg2}."
+>>>>>>> 1c4e4a5d2ef480952f8f11711c201a75863b3cb7
 end
